c4235ed8e51b08dedb6ed6b4ad0ec67e8f961732
manulzvz13 <manulzvz@gmail.com>
2025-06-19 22:29:53 -0600

    feat: Add retro color palette and fonts to style.css

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+// DOM Objects Selection
+const library = document.querySelector("#library");
+
+
 // Library Array
 const myLibrary = [];
 
@@ -15,3 +19,26 @@ function addBookToLibrary(name, author, pages, read) {
     const newBook = new Book(name, author, pages, read);
     myLibrary.push(newBook);
 }
+
+// Adding manually some books for easier testing
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
+addBookToLibrary("1984", "George Orwell", 328, false);
+
+// Function that loops through the array and displays each book on the page
+function displayBooks(myLibrary) {
+    // Clear the container first to avoid duplicates
+    library.innerHTML = "";
+    // Loop through each book on the array
+    for (const book of myLibrary) {
+        const bookDiv = document.createElement("div");
+        bookDiv.classList.add("book");
+        bookDiv.textContent = book.name;
+        library.appendChild(bookDiv);
+    }
+}
+
+displayBooks(myLibrary);
+
+
+
+
